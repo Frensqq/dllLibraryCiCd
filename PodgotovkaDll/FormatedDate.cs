39d835e8ebdd2b832d059757c7715f9a70859d17
Frensqq <liadovn@yandex.ru>
2026-05-11 19:37:58 +0300

    compair method complete

--- PodgotovkaDll/FormatedDate.cs
+++ PodgotovkaDll/FormatedDate.cs
@@ -12,8 +12,24 @@ namespace PodgotovkaDll
         public string comparingDate(DateTime date1, DateTime date2)
         {
 
-
-            return "";
+            if (date1 == null)
+            {
+                Debug.WriteLine("date1 is Null");
+                throw new ArgumentNullException("date1");
+            }
+            if (date2 == null) {
+                Debug.WriteLine("date2 is Null");
+                throw new ArgumentNullException("date2");
+            }
+            
+            DateTime maxDate = date1;
+            DateTime minDate = date2;
+            if (date1 < date2) {
+                maxDate = date2;
+                minDate = date1;
+            }
+            
+            return $"{maxDate} > {minDate}";
         }
 
         public string formatDate(DateTime date)
