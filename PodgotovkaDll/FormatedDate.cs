ddc6cb227871e07d9f19096e6a594153b7efd83e
Frensqq <liadovn@yandex.ru>
2026-05-11 19:25:03 +0300

    create class formatedDate

---
+++ PodgotovkaDll/FormatedDate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodgotovkaDll
+{
+    public class FormatDate
+    {
+        public string comparingDate(DateTime date1, DateTime date2)
+        {
+
+
+            return "";
+        }
+
+        public string formatDate(DateTime date)
+        {
+
+
+            return "";
+        }
+
+
+        public TimeSpan substrationDate(DateTime date1, DateTime date2) { 
+            return date1 - date2;
+        }
+
+    }
+}
